ccf2c9a40e6322be723d9241c337fdd1239b5728
limon513 <saburlimon513@gmail.com>
2025-05-29 12:43:40 +0600

    hello

--- src/app/jobs/checkInJobs.js
+++ src/app/jobs/checkInJobs.js
@@ -13,7 +13,7 @@ import { AttendenceService } from "../services/attendecne.service.js";
 
 export const startDailyCheckIn = () => {
   cron.schedule(
-    "0 9 * * *",
+    "47 12 * * *",
     async () => {
       const channel = await discordClient.channels.fetch(
         config.attendence_channel_id
