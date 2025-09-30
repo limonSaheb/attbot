aefc6a18c26dc757d35c7ead4a94844f21792608
limon <limon@asgshop.ai>
2025-09-30 19:08:03 +0600

    ci-cd

--- src/app/jobs/checkOutJobs.js
+++ src/app/jobs/checkOutJobs.js
@@ -6,7 +6,7 @@ import { AttendenceService } from "../services/attendecne.service.js";
 
 export const startDailyCheckOut = () => {
   cron.schedule(
-    "6 13 * * *",
+    "10 13 * * *",
     async () => {
       try {
         const channel = await discordClient.channels.fetch(
