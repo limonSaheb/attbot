463ebc8bfb6bd39902218940b5e12e74bb1b0844
limon <limon@asgshop.ai>
2025-11-12 09:47:36 +0600

    att postgres change

--- src/app/jobs/checkInJobs.js
+++ src/app/jobs/checkInJobs.js
@@ -6,7 +6,7 @@ import { attendenceView } from "../views/attendence.view.js";
 
 export const startDailyCheckIn = () => {
   cron.schedule(
-    "1 9 * * 1-6",
+    "50 9 * * 1-6",
     async () => {
       try {
         const channel = await discordClient.channels.fetch(
